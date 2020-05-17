e5e0251c0db74739e40dccb8c4fa7d0f3a69595f
IlluminatedNight <oa227r@gmail.com>
2020-05-16 22:30:18 -0500

    hangman game now has win/lose scenarios

--- hangman_project.py
+++ hangman_project.py
@@ -8,6 +8,10 @@ def get_occurrences(x, source):
 def guess(letter):
     return get_occurrences(letter, correct_word)  
 
+def already_guessed(letter):
+    global masked_word
+    return letter in masked_word
+
 def reveal_occurrences(new_char, indexes):
     string_builder = []
     global masked_word
@@ -19,28 +23,52 @@ def reveal_occurrences(new_char, indexes):
             string_builder.append(masked_word[i])
     return ''.join(string_builder)
 
-def show_ending_dialogue():
-    print("Thanks for playing!")
-    print("We'll see how well you did in the next stage")
+def win_condition_check():
+    global lives
+    global masked_word
+    cond1 = lives > 0
+    cond2 = '-' not in masked_word
+    return cond1 and cond2
+
+def show_bad_ending():
+    print("You are hanged!")
+
+def show_good_ending():
+    print("You guessed the word!")
+    print("You survived!")
 
 print('H A N G M A N') 
 print('')
-max_attempts = 8
+
+lives = 8
+win_condition = False
 choices = ['python', 'java', 'kotlin', 'javascript']
 correct_word = random.choice(choices)
 masked_word = '-' * len(correct_word)
 bad_choice_msg = "No such letter in the word"
 
 # Game Loop
-for i in range(max_attempts):
+while (lives > 0):
     print(masked_word)
     letter = input("Input a letter: ")
-    occurrences = guess(letter)
-    if len(occurrences) > 0:
-        masked_word = reveal_occurrences(letter, occurrences)
+    if already_guessed(letter):
+        print("No improvements")
+        lives -= 1
         print('')
-    else:
-        print(bad_choice_msg)
-        print('')
+    else: 
+        occurrences = guess(letter)
+        if len(occurrences) > 0:
+            masked_word = reveal_occurrences(letter, occurrences)
+            if win_condition_check():
+                win_condition = True
+                break
+            print('')
+        else:
+            print(bad_choice_msg)
+            lives -= 1
+            print('')
 
-show_ending_dialogue()
+if win_condition:
+    show_good_ending()
+else:
+    show_bad_ending()
