0e855042af28da50cf02766674f919fcb171df5c
IlluminatedNight <oa227r@gmail.com>
2020-05-17 11:16:43 -0500

    added some unit testing

--- hangman_project.py
+++ hangman_project.py
@@ -1,5 +1,9 @@
+#  MODULES
 import random
+import unittest
 
+
+#  METHODS
 def get_occurrences(x, source):
     # Use list compression to get indexes of all occurrences of x in the source
     indexPosList = [ i for i in range(len(source)) if source[i] == x ]
@@ -34,11 +38,12 @@ def show_bad_ending():
     print("You are hanged!")
 
 def show_good_ending():
-    print("You guessed the word!")
+    global correct_word
+    print(f"You guessed the word {correct_word}!")
     print("You survived!")
 
 def input_valid(user_input):
-    if len(user_input) < 1 or len(user_input) > 1:
+    if len(user_input) != 1 or user_input.isspace():
         print("You should print a single letter")
         print('')
         return False
@@ -51,9 +56,22 @@ def input_valid(user_input):
         print('')
         return False
     return True
-        
-    
 
+def test_initialize():
+    global lives
+    lives = 8
+    global win_condition
+    win_condition = False
+    global choices
+    choices = ['python', 'java', 'kotlin', 'javascript']
+    global correct_word
+    correct_word = random.choice(choices)
+    global masked_word
+    masked_word = '-' * len(correct_word)
+    global bad_choice_msg
+    bad_choice_msg = "No such letter in the word"    
+    
+#  MAIN SETUP
 print('H A N G M A N') 
 print('')
 
@@ -64,7 +82,8 @@ correct_word = random.choice(choices)
 masked_word = '-' * len(correct_word)
 bad_choice_msg = "No such letter in the word"
 
-# Game Loop
+
+#  GAME LOOP
 while (lives > 0):
     print(masked_word)
     letter = input("Input a letter: ")
@@ -83,7 +102,32 @@ while (lives > 0):
             lives -= 1
             print('')
 
+#  RESOLUTION
+
 if win_condition:
     show_good_ending()
 else:
     show_bad_ending()
+
+
+
+# UNIT TESTING
+
+print('')
+print(' ========== UNIT TESTING ========== ')
+print('')
+
+test_initialize()
+
+class TestHangmanProjectMethods(unittest.TestCase):
+
+    def test_input_valid_req_one_char(self):
+        self.assertTrue(input_valid('a'))
+        self.assertFalse(input_valid('aa'))
+        self.assertFalse(input_valid('aA'))
+        self.assertFalse(input_valid('Aa'))
+        self.assertFalse(input_valid(''))
+        self.assertFalse(input_valid(' '))
+
+if __name__ == '__main__':
+    unittest.main()
